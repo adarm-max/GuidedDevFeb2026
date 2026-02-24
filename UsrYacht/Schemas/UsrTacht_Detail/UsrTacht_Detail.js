c585f4316a5cb986b179de53d23b2dd741c1df3d
adarm-max <adar.m@winit.co.il>
2026-02-24 16:27:20 +0200

    traffic light lookup+data, sql view example, new Yacht section created

---
+++ UsrYacht/Schemas/UsrTacht_Detail/UsrTacht_Detail.js
@@ -0,0 +1,8 @@
+define('UsrTacht_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrTacht',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
